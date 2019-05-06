49c665f113819b2e51cf3cd22f53f5a3921f38b5
epedley885 <epedley885@PR33.hhs.win.fuhsd.org>
2019-05-06 12:16:23 -0700

    coding sprite

--- APCSFinal/src/Sprite.java
+++ APCSFinal/src/Sprite.java
@@ -1,4 +1,13 @@
+import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sprite {
-
+	protected float x,y,width,height;
+	private PImage img;
+	
+	public Sprite()
+	
+	public void draw(PApplet drawer) {
+		drawer.image(img, x,y,width, height);
+	}
 }
