e667d596725ee9f582706fdf0fd39ffd7d4b2d67
epedley885 <epedley885@PR33.hhs.win.fuhsd.org>
2019-05-08 11:15:40 -0700

    worked on sprite

--- APCSFinal/src/game/Sprite.java
+++ APCSFinal/src/game/Sprite.java
@@ -6,6 +6,9 @@ public class Sprite {
 	public float x,y;
 	protected float width,height;
 	private PImage img;
+	private boolean inMeleeAttack;
+	private int meleeAnimationFrames;
+	private float meleeAttackDirectionAngle;
 	
 	public Sprite(float x, float y,PImage img) {
 		this.x=x;
@@ -17,6 +20,18 @@ public class Sprite {
 	
 	public void draw(PApplet drawer) {
 		drawer.image(img, x,y,width,height);
+		if(inMeleeAttack) {
+			int maxFrames = 10;
+			float a = meleeAttackDirectionAngle;
+			float m = meleeAnimationFrames;
+			float cx = x+width/2f;
+			float cy = y+height/2f;
+			drawer.line(cx, cy, cx+200*PApplet.cos(a), cy+200*PApplet.sin(a));
+			drawer.line(cx,cy,cx+200*PApplet.cos(PApplet.PI*m/maxFrames+a-PApplet.PI/2),cy+200*PApplet.sin(PApplet.PI*m/maxFrames+a-PApplet.PI/2));
+			meleeAnimationFrames++;
+			if(m>maxFrames)
+				inMeleeAttack=false;
+		}
 	}
 	
 	public void scale(float scaleFactor) {
@@ -29,7 +44,19 @@ public class Sprite {
 		y+=dY;
 	}
 	
-	public void melee(float directionAngle) {
-		
+	public float getWidth() {
+		return width;
 	}
+	
+	public float getHeight() {
+		return height;
+	}
+	
+	public void melee(float angle) {
+		meleeAttackDirectionAngle = angle;
+		meleeAnimationFrames=0;
+		inMeleeAttack=true;
+	}
+	
+	
 }
--- APCSFinal/src/testers/EricTester.java
+++ APCSFinal/src/testers/EricTester.java
@@ -12,11 +12,21 @@ public class EricTester extends PApplet {
 	
 	public void setup() {
 		p=new Sprite(200,200,loadImage("resources\\images\\duck.png"));
+		p.scale(0.25f);
 	}
 	
 	public void draw() {
 		background(255);
 		p.draw(this);
+		noFill();
+		rect(p.x,p.y,p.getWidth(),p.getHeight());
+	}
+	
+	public void mouseClicked() {
+		p.melee(atan((mouseY-p.y)/(mouseX-p.x))+((mouseX<p.x)? PI:0));
+	}
+	
+	public void keyPressed() {
 		p.x=mouseX;
 		p.y=mouseY;
 	}
