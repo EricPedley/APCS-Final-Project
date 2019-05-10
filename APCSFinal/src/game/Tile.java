e4fb818dd84596567cb76968c754cbd37b82b755
rkoka209 <rkoka209@PR34.hhs.win.fuhsd.org>
2019-05-10 09:47:38 -0700

    New files

---
+++ APCSFinal/src/game/Tile.java
@@ -0,0 +1,24 @@
+package game;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Tile {
+	
+	private PImage image;
+	private int type;
+	
+	public Tile(PImage image, int type) {
+		this.type = type;
+		this.image = image;
+	}
+	
+	public int getType() {
+		return type;
+	}
+	
+	public void draw(PApplet p, int size) {
+		p.image(image,0,0,size,size);
+	}
+	
+}
