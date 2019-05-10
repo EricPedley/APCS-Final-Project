c2cb67bcd271fc211f5fcfcb1cad7cf6c5c13f87
owong458 <50178083+owong458@users.noreply.github.com>
2019-05-09 21:37:11 -0700

    Update Obstacle.java
    Added fields, constructor, isInside() method for checking if a point is inside the obstacle

--- APCSFinal/src/game/Obstacle.java
+++ APCSFinal/src/game/Obstacle.java
@@ -1,4 +1,20 @@
 package game;
 public class Obstacle {
-
+    private int x,y;
+    private int width,height;
+    public Obstacle(int x, int y, int width, int height){
+       this.x=x;
+        this.y=y;
+       this.width=width;
+        this.height=height;
+    }
+    
+    public boolean isInside(int x, int y){
+        if(Math.abs(this.x-x)<width/2){
+            if(Math.abs(this.y-y)<height/2){
+                return true;
+            }
+        }
+        return false;   
+    }
 }
