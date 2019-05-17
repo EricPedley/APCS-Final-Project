a914469cf2644cff4ddce77cbee072814a776da6
Generic <Generic@Menswear.attlocal.net>
2019-05-16 17:35:05 -0700

    fix checkout conflic plz

--- APCSFinal/src/game/Projectile.java
+++ APCSFinal/src/game/Projectile.java
@@ -5,11 +5,18 @@ import processing.core.PImage;
 
 public class Projectile extends Sprite {
 
+	public boolean deleteMe = false;
 	
 	public Projectile(float x, float y, PImage img, Vector velocity) {
 		super(x, y, img);
 		hitboxMode=0;
 		vel=velocity;
+	}
+	
+	@Override
+	public void handleTileCollisions(Sprite tile,PApplet debugger) {
+		if(tile.intersects(this))
+			deleteMe=true;
 	}
 	
 	
