32bc7030d0c5d9be0afa1ac1b80478de9b0e417d
Admin <Admin@DESKTOP-3AOH9CK.attlocal.net>
2019-05-20 20:41:03 -0700

    Implemented temporary start, pause, and game over screens. Does not detect if game has been won/lost yet.

--- APCSFinal/src/game/Window.java
+++ APCSFinal/src/game/Window.java
@@ -15,7 +15,7 @@ enum GameState {
 public class Window extends PApplet{
 
 	private boolean[] keys = new boolean[500];
-	private static String fS = System.getProperty("file.separator");
+	private static String fs = System.getProperty("file.separator");
 	private Player p;
 	private float startX = 300, startY = 200;
 	private Level currentLevel;
@@ -23,28 +23,28 @@ public class Window extends PApplet{
 	private GameState state;
 	//set up
 	public void setup() {
-		state = GameState.INGAME;
+		state = GameState.START;
 		currentLevel = new Level(1, 2,this);
 		currentLevel.passKeysReference(keys);
-		//
-		//INSERT MENU CONSTRUCTORS HERE
-		//
+		startScreen = new Menu(this.loadImage("resources" + fs + "images" + fs + "startSample.png"));
+		pauseScreen = new Menu(this.loadImage("resources" + fs + "images" + fs + "pauseSample.png"));
+		gameOverScreen = new Menu(this.loadImage("resources" + fs + "images" + fs + "gameoverSample.png"));
 	}
 	
 	public void draw() {
 		background(255);
 		switch(state) {
 		case START:
-			//startScreen.draw(this);
+			startScreen.draw(this);
 			break;
 		case PAUSE:
-			//pauseScreen.draw(this);
+			pauseScreen.draw(this);
 			break;
 		case INGAME:
 			currentLevel.draw(this);
 			break;
 		case GAMEOVER:
-			//gameOverScreen.draw(this);
+			gameOverScreen.draw(this);
 			break;
 		}
 		
@@ -57,6 +57,22 @@ public class Window extends PApplet{
 			if(state==GameState.START)
 			{
 				state=GameState.INGAME;
+				System.out.println("yIpeE");
+			}
+		}
+		if(keys[82])
+		{
+			if(state==GameState.INGAME)
+			{
+				state=GameState.PAUSE;
+			}
+			else if(state==GameState.PAUSE)
+			{
+				state=GameState.INGAME;
+			}
+			else if(state==GameState.GAMEOVER)
+			{
+				restart();
 			}
 		}
 		//program spec key codes for pausing, resuming, restarting, instructions, credits, etc.
@@ -70,6 +86,12 @@ public class Window extends PApplet{
 		currentLevel.mouseClicked(mouseX,mouseY, mouseButton);
 	}
 	
-
+	public void restart() {
+		state = GameState.START;
+		currentLevel = new Level(1, 2,this);
+		currentLevel.passKeysReference(keys);
+	}
+	
+	
 	
 }
