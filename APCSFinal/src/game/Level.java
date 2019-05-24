71679ace64fc1280c7752dded74b55defe5580ca
epedley885 <epedley885@PR33.hhs.win.fuhsd.org>
2019-05-24 09:11:26 -0700

    fixing checkout conflict

--- APCSFinal/src/game/Level.java
+++ APCSFinal/src/game/Level.java
@@ -24,6 +24,7 @@ public class Level {
 	private boolean[] keys;
 	private float mouseX, mouseY;
 	private float startX = 650, startY = 350;
+	
 
 	// Constuctors
 	public Level(int levelNumber, int numEnemies, PApplet loader) {
