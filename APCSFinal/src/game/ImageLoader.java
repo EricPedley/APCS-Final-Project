a2580053660bf4fc2279c613b518f8593ab7d716
Rishab Koka <Rishab Koka@DESKTOP-9FI92E4>
2019-05-23 21:31:25 -0700

    fix it

--- APCSFinal/src/game/ImageLoader.java
+++ APCSFinal/src/game/ImageLoader.java
@@ -18,11 +18,8 @@ public class ImageLoader {
 		Projectile = p.loadImage("resources" + fs + "images" + fs + "Projectile.gif");
 		tileIndices[0] = new Tile(p.loadImage("resources" + fs + "images" + fs + "Wall.gif"),0);
 		tileIndices[1] = new Tile(p.loadImage("resources" + fs + "images" + fs + "Floor.gif"),1);
-<<<<<<< HEAD
 		tileIndices[2] = new Tile(p.loadImage("resources" + fs + "images" + fs + "BottomLeft.gif"),2);
 		tileIndices[3] = new Tile(p.loadImage("resources" + fs + "images" + fs + "BottomRight.gif"),3);
-=======
->>>>>>> branch 'master' of https://github.com/EricPedley/APCS-Final-Project.git
 		tileIndices[4] = new Tile(p.loadImage("resources" + fs + "images" + fs + "TopLeft.gif"),4);
 		tileIndices[5] = new Tile(p.loadImage("resources" + fs + "images" + fs + "TopRight.gif"),5);
 		tileIndices[2] = new Tile(p.loadImage("resources" + fs + "images" + fs + "BottomRight.gif"),3);
