75a3c280f12c9c7b4388134ac1f0f0cf0802d26f
owong458 <owong458@PR35.hhs.win.fuhsd.org>
2019-05-24 10:30:55 -0700

    Updated level select

--- APCSFinal/src/game/Window.java
+++ APCSFinal/src/game/Window.java
@@ -30,7 +30,7 @@ public class Window extends PApplet{
 		gameOverScreen = new Menu(this.loadImage("resources" + fs + "images" + fs + "gameoverSample.png"));
 		instructionScreen = new Menu(this.loadImage("resources" + fs + "images" + fs + "instructions3.png"));
 		winScreen = new Menu(this.loadImage("resources" + fs + "images" + fs + "winSample.png"));
-		levelSelect= new Menu(this.loadImage("resources" + fs + "images" + fs + "Level1.png"));
+		levelSelect= new Menu(this.loadImage("resources" + fs + "images" + fs + "level3.png"));
 	}
 	
 	public void draw() {
